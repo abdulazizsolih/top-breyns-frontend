928774391ee89530de9791666c47d645644bb13c
Hasandesign <51147185+Hasan-prog@users.noreply.github.com>
2021-09-07 16:38:35 +0500

    Environmental prepared

---
+++ src/components/components.js
@@ -0,0 +1,5 @@
+import {Head} from "@vueuse/head";
+
+export default [
+    Head
+]
--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import { createHead } from '@vueuse/head';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import components from "./components/components";
+import '@/assets/scss/app.scss';
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+const head = createHead()
+
+// Registering components
+components.forEach(component => {
+    app.component(component.name, component)
+})
+
+app
+    .use(head)
+    .use(store)
+    .use(router)
+    .mount('#app')
